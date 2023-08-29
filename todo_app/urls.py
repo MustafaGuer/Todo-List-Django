d59ce51cb5a79b4925ee58661f1e1c35bbac74ee
MustafaGuer <mustafaguer@outlook.com>
2023-08-29 09:46:15 +0200

    add listview & conf routes

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,3 +1,7 @@
-urlpatterns = [
+from django.urls import path
+from . import views
 
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+    path("list/<int:list_id>", views.ItemView.as_view(), name="list")
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,8 +1,21 @@
 from django.views.generic import ListView
 
-from .models import TodoList
+from .models import TodoList, TodoItem
 
 
-class ListView(ListView):
+class IndexView(ListView):
     model = TodoList
     template_name = "todo_app/index.html"
+
+
+class ItemView(ListView):
+    model = TodoItem
+    template_name = "todo_app/todo_list.html"
+
+    def get_queryset(self):
+        return TodoItem.objects.filter(todo_list_id=self.kwargs["list_id"])
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context["todo_list"] = TodoList.objects.get(id=self.kwargs["list_id"])
+        return context
